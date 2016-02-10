0e20f03c83635e461b3dcfbabae7a2294f057e0a
Jeroen Diederix <jeroen.diederix@zivver.com>
2016-02-10 11:30:42 +0100

    test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Raml.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var file = @"C:\Users\Jeroen\Documents\zivver-api\src\api.raml";
+
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(file));
+
+            var parser = new RamlParser().LoadAsync(file).Result;
+
+            Console.WriteLine("The end...");
+
+        }
+    }
+}
